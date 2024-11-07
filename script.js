958f3e5d55dbd6a0c4d3e4b0c6307429492c7385
Ahmad-U19 <ahmedoppal2592@gmail.com>
2024-11-07 22:26:12 +0500

    Final Code

---
+++ script.js
@@ -0,0 +1,18 @@
+var x=32;
+var b;
+
+if(x >=80 && x<=100)
+{
+    document.write("Congrats in Merit")
+} 
+else if(x >=60 && x<80 )
+{
+    document.write("1st Division")
+}
+else{
+    document.write("Enter Valid Input")
+}
+
+(x==32)? b=true : b=false;
+document.write("<br>");
+document.write(b);
